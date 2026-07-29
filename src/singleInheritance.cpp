c134c9221472251bccbb8c922006960f612ff61a
Jayanthi <jayanthi26032006@gmail.com>
2026-07-29 23:07:06 +0530

    Added singleInheritance.cpp

---
+++ src/singleInheritance.cpp
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class abc
+{
+    public :
+    void operations(int a,int b)
+    {
+        cout<<a+b<<std::endl;
+    }
+   
+};
+class abcd:public abc
+{
+    public :
+    void operation(int a,int b)
+    {
+        cout<<abs(a-b)<<std::endl;
+    }
+   
+};
+int main()
+{
+    
+   abcd it;
+   it.operations(5,6);
+   it.operations(15,7);
+   return 0;
+}
